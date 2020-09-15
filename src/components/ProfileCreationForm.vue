5deea1001c766009b973258e524d024a561e3816
Aurlin Dhillon <aurlindhillon@gmail.com>
2020-09-14 17:19:35 -0700

    created basic form with submit structure for initial landing of page

--- src/components/ProfileCreationForm.vue
+++ src/components/ProfileCreationForm.vue
@@ -1,12 +1,44 @@
 <template>
-  <div>
-    profile creation form
-  </div>
+  <v-form>
+    <v-container>
+      <v-col>
+        <v-text-field
+          v-model="firstname"
+          label="First name"
+          required
+        ></v-text-field>
+      </v-col>
+
+      <v-col>
+        <v-text-field
+          v-model="lastname"
+          label="Last name"
+          required
+        ></v-text-field>
+      </v-col>
+
+      <v-col>
+        <v-text-field
+          v-model="bio"
+          :counter="140"
+          label="Short-bio"
+          required
+        ></v-text-field>
+      </v-col>
+
+      <v-btn class="mr-4" @click="submit">submit</v-btn>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
 export default {
   name: "ProfileCreationForm",
+  data: () => ({
+    firstname: "",
+    lastname: "",
+    bio: "",
+  }),
 };
 </script>
 
