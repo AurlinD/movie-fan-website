21517ed8cad6749a513d7d30401c51ea684e2d4e
Aurlin Dhillon <aurlindhillon@gmail.com>
2020-09-17 21:45:37 -0700

    movieItem tile turns red/green depending on rating

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-card class="mx-auto" max-width="344" outlined>
+  <v-card class="mx-auto" v-bind:class="[color ? 'green' : 'red']" max-width="344" outlined>
     <v-list-item three-line>
       <v-list-item-content>
         <v-list-item-title class="headline mb-1">{{movie.title}}</v-list-item-title>
-        <v-list-item-subtitle>Release Date: {{movie.releaseDate}}</v-list-item-subtitle>
+        <v-list-item-subtitle>'Release Date: {{movie.releaseDate}}</v-list-item-subtitle>
         <v-list-item-subtitle>Score: {{movie.userScore}}</v-list-item-subtitle>
         <v-list-item-text>{{movie.overview}}</v-list-item-text>
       </v-list-item-content>
@@ -15,8 +15,23 @@
 export default {
   name: "MovieItem",
   props: ["movie"],
+  computed: {
+    color() {
+      return this.movie.userScore >= 8;
+    },
+  },
 };
 </script>
 
 <style scoped>
+.mx-auto {
+  margin: 1em;
+}
+
+.green {
+  background-color: lightgreen;
+}
+.red {
+  background-color: lightcoral;
+}
 </style>
