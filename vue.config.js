d94bf97c3b97c6f28d827ca00bac8a1ebe14641c
Aurlin Dhillon <aurlindhillon@gmail.com>
2020-09-19 17:00:34 -0700

    adding deployment files for github pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/movie-fan-website/" : "/",
+  transpileDependencies: ["vuetify"],
+};
